defcb2fad1b4699e2c4e3779c3bf4ae3aee2446f
defivault <67746633+defivault@users.noreply.github.com>
2020-07-19 04:18:08 +0200

    Delete ILendingPoolAddressesProvider.sol

--- ILendingPoolAddressesProvider.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.6.6;
-
-interface ILendingPoolAddressesProvider {
-    function getLendingPoolCore() external view returns (address payable);
-    function getLendingPool() external view returns (address);
-}
